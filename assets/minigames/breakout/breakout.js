e872ce311e940b857cfe743175d5a5e770056389
Andrew Dempsey <andrewdempsey2016@gmail.com>
2021-08-07 17:42:09 +0100

    Added comments to Breakout code

--- assets/minigames/breakout/breakout.js
+++ assets/minigames/breakout/breakout.js
@@ -1,3 +1,5 @@
+/* initialise Phaser library. Set the screen size and add physics to the game world */
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -18,15 +20,19 @@ var config = {
     }
 };
 
+/* game is an instance of a Phaser scene */
 var game = new Phaser.Game(config);
 
+/* two states, ball moving and lsing state where player launches ball */
 const gameState = {
     PLAY: "play",
     PREPARE: "prepare",
 }
 
+/* initial state, player will launch ball */
 let state = gameState.PREPARE;
 
+/* load graphics assets, sounds and initialise player input variables */
 function preload() {
     this.load.image('ball', 'assets/ball.png');
     this.load.image('paddle', 'assets/paddle.png');
@@ -49,8 +55,10 @@ function preload() {
     this.load.audio('hit', 'assets/hit.wav');
 }
 
+/* BAD CODE (maybe?) the hit sound is a global variable so it can be used in the "killBrick function */
 var hit;
 
+/* initialise the level, assets and physics of the game world */
 function create() {
 
     hit = this.sound.add('hit');
@@ -71,25 +79,33 @@ function create() {
 
     let level = levelData.bricks.map(brick => bricks.create(brick.x, brick.y, brick.name))
 
+    /* when a collision is detected between the ball and a brick, the killBrick function fires
+    the ball bounces, the brick is destroyed and the 'hit' sound plays */
     this.physics.add.collider(ball, bricks, killBrick, null, this);
 
 }
 
+/* handle things that happen constantly suring the game loop such as player input and moving sprites */
 function update() {
     if (keyLeft.isDown) {
-        paddle.x -= 3;
+        paddle.x -= 3; //horizontal position of the paddle is reduced
     }
 
     if (keyRight.isDown) {
-        paddle.x += 3;
+        paddle.x += 3; //horizontal position of the paddle is increased
     }
 
+    /* vertical position of the ball is under the paddle, so the player has missed the ball,
+       trigger game lose state */
     if (ball.y > paddle.y) {
         state = gameState.PREPARE;
         console.log("Player dropped the ball");
         ball.y = 500;
     }
 
+    /* game lose state, ball velocity is zero, ball is fixed to the position of the paddle so the
+       player can pick where to launch. Player launches the ball with spacebar, thereby setting the balls velocity
+       to maximum */
     if (state === gameState.PREPARE) {
         ball.setVelocityX(0);
         ball.setVelocityY(0);
@@ -108,6 +124,7 @@ function update() {
     }
 }
 
+/* called when a collision is detected between the ball and a brick */
 function killBrick(ball, brick) {
     brick.disableBody(true, true);
     ball.setBounceY(1);
